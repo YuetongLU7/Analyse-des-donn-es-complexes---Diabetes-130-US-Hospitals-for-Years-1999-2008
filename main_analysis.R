efc128fb09e5d8551c6c6992beaf1c428288b62e
Yuetong <yuetong1113lu@gmail.com>
2025-11-04 14:03:18 +0100

    [ADD] file of main analysis

---
+++ main_analysis.R
@@ -0,0 +1,8 @@
+# Project: Diabetes Hospital Dataset Analysis
+# Authors: Yuetong Lu & Guilherme Peres Tatagiba
+
+library(FactoMineR)
+
+diabetes <- read.csv("dataset/diabetic_data.csv")
+
+View(diabetes)
